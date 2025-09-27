0ace7575bdeda13027dec7fd10b3e495d8adb98c
daniilbait <d.baytalenko@gmail.com>
2025-09-27 12:27:51 +0000

    Number 3-7

--- main.cpp
+++ main.cpp
@@ -2,13 +2,16 @@
 #include <string>
 #include <fstream>
 #include <cstdlib>
+#include <sstream>
 int main() {
-  const char* start_dir = getenv("HOME");
-  std::string file_path = std::string(start_dir)+"/kubsh_history";
-
-
+  const char* start_dir = std::getenv("HOME");
+  std::string start_path = std::string(start_dir)+"/kubsh_history";
+  const char* PATH_dir = std::getenv("PATH");
+  
+ 
+  
   std::string input;
-  std::ofstream output(file_path, std::ios::app);
+  std::ofstream output(start_path, std::ios::app);
   while(std::getline(std::cin, input))
 	{
 	output<<input<<"\n";
@@ -18,7 +21,19 @@ int main() {
 		case '/':
 		{
 			if(input=="/q") {output.close(); return 0;}
-			else std::cout<<input<<"\n";
+			else if (input=="/e $PATH") 
+			{
+				std::string path_str(PATH_dir);
+				std::stringstream ss(path_str);
+				std::string directory;
+				std::cout<<"Directories in PATH:\n";
+				int count=1;
+				while(std::getline(ss, directory, ':'))
+				{
+					std::cout<<count<<". "<<directory<<"\n";
+					count++;
+				}
+			}
 			break;
 		}
 		case 'e':

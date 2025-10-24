c2796050fc603ed1be8e52dab4658241a58368fe
daniilbait <d.baytalenko@gmail.com>
2025-10-24 09:38:57 +0300

    Number 1

--- main.cpp
+++ main.cpp
@@ -4,45 +4,8 @@
 #include <cstdlib>
 #include <sstream>
 int main() {
-  const char* start_dir = std::getenv("HOME");
-  std::string start_path = std::string(start_dir)+"/kubsh_history";
-  const char* PATH_dir = std::getenv("PATH");
-  
- 
-  
-  std::string input;
-  std::ofstream output(start_path, std::ios::app);
-  while(std::getline(std::cin, input))
-	{
-	output<<input<<"\n";
-	char letter=input.at(0);
-	switch(letter)
-		{
-		case '/':
-		{
-			if(input=="/q") {output.close(); return 0;}
-			else if (input=="/e $PATH") 
-			{
-				std::string path_str(PATH_dir);
-				std::stringstream ss(path_str);
-				std::string directory;
-				std::cout<<"Directories in PATH:\n";
-				int count=1;
-				while(std::getline(ss, directory, ':'))
-				{
-					std::cout<<count<<". "<<directory<<"\n";
-					count++;
-				}
-			}
-			break;
-		}
-		case 'e':
-		{
-			if(input=="echo") std::cout<<123<<"\n";
-			break;
-		}
-		default:{std::cout<<"There is no such command!\n";break;}
-		}
-	}
-  output.close();
+  std::string a;
+  std::cin>>a;
+  std::cout<<a<<"\n";
+  return 0;
 }

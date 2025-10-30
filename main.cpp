74aae2c5384e7ef13d06d04c66693a06bce7bd6b
daniilbait <d.baytalenko@gmail.com>
2025-10-30 09:41:50 +0000

    Backup of the Number 1-5

--- main.cpp
+++ main.cpp
@@ -2,10 +2,32 @@
 #include <string>
 #include <fstream>
 #include <cstdlib>
-#include <sstream>
 int main() {
-  std::string a;
-  std::cin>>a;
-  std::cout<<a<<"\n";
-  return 0;
+  const char* start_dir = getenv("HOME");
+  std::string file_path = std::string(start_dir)+"/kubsh_history";
+
+
+  std::string input;
+  std::ofstream output(file_path, std::ios::app);
+  while(std::getline(std::cin, input))
+	{
+	output<<input<<"\n";
+	char letter=input.at(0);
+	switch(letter)
+		{
+		case '/':
+		{
+			if(input=="/q") {output.close(); return 0;}
+			else std::cout<<input<<"\n";
+			break;
+		}
+		case 'e':
+		{
+			if(input=="echo") std::cout<<123<<"\n";
+			break;
+		}
+		default:{std::cout<<input<<"\n";break;}
+		}
+	}
+  output.close();
 }
